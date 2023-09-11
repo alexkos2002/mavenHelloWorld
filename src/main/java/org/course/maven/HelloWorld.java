0de5538a9c3e101b71ab892c1e759b8068232d0c
Kosiuk, Oleksii <Oleksii.Kosiuk@McKesson.com>
2023-09-11 04:07:08 +0300

    Initial commit

---
+++ src/main/java/org/course/maven/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.course.maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(StringUtils.capitalize("Hello, World!"));
+    }
+}
